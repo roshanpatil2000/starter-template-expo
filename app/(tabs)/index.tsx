91cbce692f1a36232d106cda6f059d8249d0f97e
Roshan Patil <roshan@Roshans-MacBook-Pro.local>
2025-08-15 11:28:29 +0530

    added glueStack and dark light theme

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,39 @@
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { View } from 'react-native';
+import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
+import { StatusBar } from 'expo-status-bar';
+import { Platform, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ThemedView className= "flex-1 justify-center item-center border-[10px] border-green-300 ">
-      <View className='h-10  border-[3px] border-lime-500'>
-      <ThemedText className={styles.text}>Helllo, World!</ThemedText>
-      </View>
-      <ThemedText className={styles.text}>Helllo, World!</ThemedText>
-    </ThemedView>
+    <View className={Platform.OS === "ios" ? "mt-12 px-2" : "mt-4 px-2"}>
+      <StatusBar hidden />
+      <VStack className="justify-between" >
+        <Text className='text-2xl color-tertiary-0 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-50 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-100 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-200 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-300 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-400 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-500 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-600 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-700 font-bold'>Home</Text>
+        <Text className='text-2xl color-tertiary-800 font-bold'>Text</Text>
+        <Text className='text-2xl color-tertiary-900 font-bold'>Text</Text>
+        <Text className='text-2xl color-tertiary-950 font-bold'>Text</Text>
+        <Text className='text-2xl color-typography-0 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-50 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-100 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-200 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-300 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-400 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-500 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-600 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-700 font-bold'>Home</Text>
+        <Text className='text-2xl color-typography-800 font-bold'>Text</Text>
+        <Text className='text-2xl color-typography-900 font-bold'>Text</Text>
+        <Text className='text-2xl color-typography-950 font-bold'>Text</Text>
+      </VStack>
+    </View>
   );
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -17,13 +19,14 @@ export default function RootLayout() {
     return null;
   }
 
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode={"system"}><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </ThemeProvider></GluestackUIProvider>
   );
 }
